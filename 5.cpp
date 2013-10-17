0fd6bad83b83ed3e63e02de5cfc9f558ebde3319
Josue Mora <josue@inno-live.com>
2013-10-17 04:07:15 -0700

    Reading from file, removing unwanted spaces

--- 5.cpp
+++ 5.cpp
@@ -4,56 +4,110 @@
 //	Project: 	5
 //	Date:    	October 17, 2013
 //	Professor:  Ray Ahmadnia
-//	Purpose: 	Program reads c++ code from a file and removes 
-//              comment lines, unwanted spaces, then places a 
+//	Purpose: 	Program reads c++ code from a file and removes
+//              comment lines, unwanted spaces, then places a
 //              space between all tokens
 //==============================================================
 
 #include <iostream>
-#include <fstream>
-#include <cstring>
-#include <string>
 #include <vector>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <cctype>
+
 
 using namespace std;
 
-class CLEAN{
+class STRIP{
 		private:
 				fstream f;
-				string token;
-				string temp_token, temp_string;
-				vector <string> input;
-				vector <string> input_st;
-				vector<string>::iterator it;
-				int size, comma, period, semicolon;
-				int counter, p;
-				
-				bool isIdentifier;
-				bool isInteger;
-				bool isRealNumber;
-				bool isReservedWord;
-				
+				vector <string> T;
+				vector <string> X;
+				vector <string>::iterator iter;
+				string line;
+				string s;
+				int n;
+
 		public:
 				void readData(){
-						f.open("data.txt", ios::in);
-						if(!f){
+					f.open("data.txt", ios::in);
+					if(!f){
 						cout << "File not found";
-						}
-						while( !f.eof() ){
-								cout << "hello" << endl;
-								//f >> token;
-								//input.push_back(token);
-						}
+					}
+					while(getline(f, line)){
+						T.push_back(line);
+					}
 				}
-				void removeComments(){}
-				void removeSpaces(){}
+				void cleanData(){
 				
+					for (iter = T.begin(); iter != T.end(); iter++){
+						s = *iter;
+						n = s.length();
+						for(int j = 0; j < 3; j++){
+							for(int i = 0; i < n; ++i){
+								// remove extra spaces
+								if((s[i] == ' ')&&(s[i+1] == ' ')){
+									s.erase(i,1);
+								}
+								// remove spaces between parentheses
+								else if((s[i] == '(')&& (s[i+1] == ' ')){
+									s.erase(i+1,1);
+								}
+								// remove spaces before comma
+								else if((s[i] == ' ')&& (s[i+1] == ',')){
+									s.erase(i,1);
+								}
+								// remove spaces after comma
+								else if((s[i] == ',')&& (s[i+1] == ' ')){
+									s.erase(i+1,1);
+								}
+								// remove spaces before semicolon
+								else if((s[i] == ' ')&& (s[i+1] == ';')){
+									s.erase(i,1);
+								}
+								// remove comments
+								else if((s[i] == '/')&&(s[i+1] == '/')){
+									s.erase(i,i+n);
+								}
+								// remove single periods
+								else if(s[i] == '.'){
+									s.erase(i,1);
+								}
+								// remove single lines
+								else if(s[n] == '\n'){
+									cout << "blank line" << endl;
+								}
+							}
+						}
+						cout << s << endl;
+					}
+				}
+				
+				void printData(){
+					vector <string>::iterator iter;
+					for (iter = T.begin(); iter != T.end(); iter++){
+    					cout << *iter << endl << endl;
+					}
+				}
+				void printLineLength(){
+					int counter = 1;
+					vector <string>::iterator iter;
+					for (iter = T.begin(); iter != T.end(); iter++){
+						string temp = *iter;
+						int y = temp.length();
+						cout << counter << '\t' << y << endl;
+					}
+				}
+
 };
 
 int main(){
 
-CLEAN A;
+STRIP A;
 A.readData();
+//A.printLineLength();
+A.cleanData();
 
 return(0);
 }
